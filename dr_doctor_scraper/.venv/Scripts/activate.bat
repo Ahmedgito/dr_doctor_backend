2d867d63e6d081fe0e48c8cc20c217a6b38889af
HsmS21 <hsturbo21@gmail.com>
2025-12-03 01:45:03 +0500

    changes to venv

--- dr_doctor_scraper/.venv/Scripts/activate.bat
+++ dr_doctor_scraper/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ahmedabid\dr_doctor_backend\dr_doctor_scraper\.venv
+set VIRTUAL_ENV=D:\projacts\dr_doctor_backend\dr_doctor_backend\dr_doctor_scraper\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
